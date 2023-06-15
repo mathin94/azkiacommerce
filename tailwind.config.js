7aa0269f7616650fe257501ec9987a546adfada2
Mathin Mochammad <mathin.mochammad@gmail.com>
2023-06-15 19:49:18 +0700

    fix view order

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 import colors from "tailwindcss/colors";
 import forms from "@tailwindcss/forms";
 import typography from "@tailwindcss/typography";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
     content: ["./resources/**/*.blade.php", "./vendor/filament/**/*.blade.php"],
     darkMode: "class",
     theme: {
         extend: {
+            fontFamily: {
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+            },
             colors: {
                 danger: colors.rose,
                 primary: colors.blue,
